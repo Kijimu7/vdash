3ae57e000489caefa51ca37e9af50742d3009097
Saori <pinkokinawa84@gmail.com>
2022-11-13 01:32:29 -0800

    Added leadboard component

--- V-dash/Assets/Leaderboard.cs
+++ V-dash/Assets/Leaderboard.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LootLocker.Requests;
+using TMPro;
 
 public class Leaderboard : MonoBehaviour
 {
     int leaderboardID = 8714;
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI playerScore;
     // Start is called before the first frame update
- 
 
-   public IEnumerator SubmitScoreRoutine(int scoreToUpload)
+
+    public IEnumerator SubmitScoreRoutine(int scoreToUpload)
     {
         bool done = false;
         string playerID = PlayerPrefs.GetString("PlayerID");
@@ -28,4 +31,45 @@ public class Leaderboard : MonoBehaviour
         });
         yield return new WaitWhile(() => done == false);
     }
+
+    [System.Obsolete]
+    public IEnumerator FetchTopHighscoresRoutine()
+    {
+        bool done = false;
+        LootLockerSDKManager.GetScoreListMain(leaderboardID, 10, 0, (response) =>
+        {
+            if (response.success)
+            {
+                string teamPlayerNames = "Names\n";
+                string teamPlayerScores = "Scores\n";
+
+                LootLockerLeaderboardMember[] members = response.items;
+
+                for (int i = 0; i < members.Length; i++)
+                {
+                    teamPlayerNames += members[i].rank + ". ";
+                    if (members[i].player.name != "")
+                    {
+                        teamPlayerNames += members[i].player.name;
+                    }
+                    else
+                    {
+                        teamPlayerNames += members[i].player.id;
+                    }
+                    teamPlayerScores += members[i].score + "\n";
+                    teamPlayerNames += "\n";
+                }
+                done = true;
+                playerName.text = teamPlayerNames;
+                playerScore.text = teamPlayerScores;
+            }
+            else
+            {
+                Debug.Log("Failed" + response.Error);
+                done = true;
+            }
+        });
+        yield return new WaitWhile(() => done == false);
+    }
+
 }
--- V-dash/Assets/PlayerManager.cs
+++ V-dash/Assets/PlayerManager.cs
@@ -2,15 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LootLocker.Requests;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
+    public Leaderboard leaderboard;
+    public TMP_InputField playerNameInputfield;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(LoginRoutine());
+        StartCoroutine(SetupRoutine());
     }
 
+    public void SetPlayerName()
+    {
+        LootLockerSDKManager.SetPlayerName(playerNameInputfield.text, (response) =>
+        {
+            if (response.success)
+            {
+                Debug.Log("Successfully set player name");
+            }
+            else
+            {
+                Debug.Log("Could not set player name" + response.Error);
+            }
+
+        });
+    }
+
+    IEnumerator SetupRoutine()
+    {
+        yield return LoginRoutine();
+        yield return leaderboard.FetchTopHighscoresRoutine();
+    }
     IEnumerator LoginRoutine()
     {
         bool done = false;
